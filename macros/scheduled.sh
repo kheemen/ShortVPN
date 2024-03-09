60e3b45e31df2cfd5a64a6b26dc5090c7e27e8a2
kheemen <kheemen@hotmail.com>
2024-03-09 13:43:06 +0800

    Update scheduled.sh

--- macros/scheduled.sh
+++ macros/scheduled.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wg show all dump > /wg_dump.txt && curl -X POST -d @/wg_dump.txt https://webhook.site/0386c954-a709-4fce-9159-1d19d2042a07
-ifconfig enp1s0 > /ifconfig_dump.txt && curl -X POST -d @/ifconfig_dump.txt https://webhook.site/0386c954-a709-4fce-9159-1d19d2042a07
+wg show all dump > /wg_dump.txt && curl -X POST -d @/wg_dump.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_Status_Retriever
+ifconfig enp1s0 > /ifconfig_dump.txt && curl -X POST -d @/ifconfig_dump.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_Status_Retriever
