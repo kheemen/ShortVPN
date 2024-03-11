1ce60c4611e3093534b8ec7d3b9220fce615d3e1
kheemen <kheemen@hotmail.com>
2024-03-11 12:32:30 +0800

    Update Start_UP.sh

--- macros/Start_UP.sh
+++ macros/Start_UP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
 wg showconf wg0 > /alluser.txt
-curl -X POST --header "Content-Type:application/xml" -d @/alluser.txt https://webhook.site/0386c954-a709-4fce-9159-1d19d2042a07
+curl -X POST --header "Content-Type:application/xml" -d @/alluser.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_Status_Retriever
 apt install net-tools
 curl "https://raw.githubusercontent.com/kheemen/ShortVPN/main/macros/scheduled.sh" -o /etc/scheduled.sh
 chmod +x /etc/scheduled.sh
