aff7c510b8112b008baa8b22460f3a1f8df9c9b7
kheemen <kheemen@hotmail.com>
2024-03-11 12:56:29 +0800

    Update scheduled.sh

--- macros/scheduled.sh
+++ macros/scheduled.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wg show all dump > /wg_dump.txt && curl -X POST -d @/wg_dump.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_Status_Retriever
-ifconfig enp1s0 > /ifconfig_dump.txt && curl -X POST -d @/ifconfig_dump.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_Status_Retriever
+wg show all dump > /wg_dump.txt && curl -X POST -H "Content-Type: text/plain" --data-binary @/wg_dump.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_Status_Retriever
+ifconfig enp1s0 > /ifconfig_dump.txt && curl -X POST -H "Content-Type: text/plain" --data-binary @/ifconfig_dump.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_Status_Retriever
