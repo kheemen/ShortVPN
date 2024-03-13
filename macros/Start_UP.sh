a8d31d6e9760b3d35aa9ed1fba28721c117d285f
kheemen <kheemen@hotmail.com>
2024-03-14 04:06:23 +0800

    Update Start_UP.sh

--- macros/Start_UP.sh
+++ macros/Start_UP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
-wg showconf wg0 > /alluser.txt
-curl -X POST --header "Content-Type:application/xml" -d @/alluser.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_Status_Retriever
+cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
+curl -X POST --header "Content-Type: text/plain" --data-binary @/alluser.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_WG_Config_Retriever
 apt install net-tools
 curl "https://raw.githubusercontent.com/kheemen/ShortVPN/main/macros/scheduled.sh" -o /etc/scheduled.sh
 chmod +x /etc/scheduled.sh
