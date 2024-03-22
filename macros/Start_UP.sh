e3866073043ed21ed5917992c2bd763198b3bf4c
kheemen <kheemen@hotmail.com>
2024-03-23 03:59:01 +0800

    Update Start_UP.sh

--- macros/Start_UP.sh
+++ macros/Start_UP.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+apt install net-tools
 sudo systemctl stop unattended-upgrades
 sudo apt remove unattended-upgrades -y
 curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
 cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
 curl -X POST --header "Content-Type: text/plain" --data-binary @/alluser.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_WG_Config_Retriever
-apt install net-tools
 curl "https://raw.githubusercontent.com/kheemen/ShortVPN/main/macros/scheduled.sh" -o /etc/scheduled.sh
 chmod +x /etc/scheduled.sh
 (crontab -l 2>/dev/null; echo "*/5 * * * * /etc/scheduled.sh") | crontab -
