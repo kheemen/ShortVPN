662f000d954d34fa886493cb865b726b1c4777bf
kheemen <kheemen@hotmail.com>
2024-03-23 15:45:44 +0800

    Update Start_UP.sh

--- macros/Start_UP.sh
+++ macros/Start_UP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt install net-tools
 sudo systemctl stop unattended-upgrades
 sudo apt remove unattended-upgrades -y
+apt install net-tools
 curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
 cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
 curl -X POST --header "Content-Type: text/plain" --data-binary @/alluser.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_WG_Config_Retriever
