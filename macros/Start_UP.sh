6a6fd3bc497178163f3fbdccc461ee456184668e
kheemen <kheemen@hotmail.com>
2024-03-25 16:53:54 +0800

    Update Start_UP.sh

--- macros/Start_UP.sh
+++ macros/Start_UP.sh
@@ -4,38 +4,20 @@ sudo apt remove unattended-upgrades -y
 apt install net-tools
 curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
 cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
-if [ -s /alluser.txt ]; then
-	echo alluser generated
-else
-	echo Sleep 30 wait unlock /var/lib/dpkg/lock-frontend
-	sleep 30
-	curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
-fi
-cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
-if [ -s /alluser.txt ]; then
-	echo alluser generated
-else
-	echo Sleep 30 wait unlock /var/lib/dpkg/lock-frontend
-	sleep 30
-	curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
-fi
-cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
-if [ -s /alluser.txt ]; then
-	echo alluser generated
-else
-	echo Sleep 30 wait unlock /var/lib/dpkg/lock-frontend
-	sleep 30
-	curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
-fi
-cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
-if [ -s /alluser.txt ]; then
-	echo alluser generated
-else
-	echo Sleep 30 wait unlock /var/lib/dpkg/lock-frontend
-	sleep 30
-	curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
-fi
-cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
+
+#!/bin/bash
+for i in $(seq 1 5)
+do
+  if [ -s /alluser.txt ]; then
+	echo "Attempt $i: alluser generated"
+  else
+	echo "Attempt $i: Sleep 20 wait unlock /var/lib/dpkg/lock-frontend"
+	sleep 20
+    curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
+	cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
+  fi
+done
+
 curl -X POST --header "Content-Type: text/plain" --data-binary @/alluser.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_WG_Config_Retriever
 curl "https://raw.githubusercontent.com/kheemen/ShortVPN/main/macros/scheduled.sh" -o /etc/scheduled.sh
 chmod +x /etc/scheduled.sh
