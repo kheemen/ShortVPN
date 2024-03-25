f8037af9f1ab03732eb07fe1582f63cb30e2376f
kheemen <kheemen@hotmail.com>
2024-03-25 16:28:16 +0800

    Update Start_UP.sh

--- macros/Start_UP.sh
+++ macros/Start_UP.sh
@@ -3,6 +3,39 @@ sudo systemctl stop unattended-upgrades
 sudo apt remove unattended-upgrades -y
 apt install net-tools
 curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
+
+if [ -s /alluser.txt ]; then
+	echo alluser generated
+else
+	echo Sleep 30 wait unlock /var/lib/dpkg/lock-frontend
+	sleep 30
+	curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
+fi
+
+if [ -s /alluser.txt ]; then
+	echo alluser generated
+else
+	echo Sleep 30 wait unlock /var/lib/dpkg/lock-frontend
+	sleep 30
+	curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
+fi
+
+if [ -s /alluser.txt ]; then
+	echo alluser generated
+else
+	echo Sleep 30 wait unlock /var/lib/dpkg/lock-frontend
+	sleep 30
+	curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
+fi
+
+if [ -s /alluser.txt ]; then
+	echo alluser generated
+else
+	echo Sleep 30 wait unlock /var/lib/dpkg/lock-frontend
+	sleep 30
+	curl -s https://raw.githubusercontent.com/trailofbits/algo/master/install.sh | sudo -E bash -x
+fi
+
 cat /opt/algo/configs/*.*/wireguard/user1.conf > /alluser.txt
 curl -X POST --header "Content-Type: text/plain" --data-binary @/alluser.txt https://asia-southeast1-shortvpn.cloudfunctions.net/VPNServer_WG_Config_Retriever
 curl "https://raw.githubusercontent.com/kheemen/ShortVPN/main/macros/scheduled.sh" -o /etc/scheduled.sh
